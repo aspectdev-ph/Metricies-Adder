5aaf1a12357cbc865e248ef02340f7bb2e13b797
Aspect <>
2020-06-05 20:38:12 +0800

    Second commit:Added loop to make your calculation continues adding options y to continue to use the program and if you are done your calculation you have an option to do another calculation or exit

--- src/metricies/adder/MetriciesAdder.java
+++ src/metricies/adder/MetriciesAdder.java
@@ -18,38 +18,52 @@ public class MetriciesAdder {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.println("Do you want to do another calculation? If yes enter Y otherwise enter any character.");
-            String conf = sc.next();
-            if (conf.equals("Y") || conf.equals("y")) {
-                System.out.println("Enter matrix data seperated with space( each matrix require 4 digit required).You should enter the digit from left to right");
-                System.out.println("Matrix A:");
-                int matrix1_1 = sc.nextInt();
-                int matrix1_2 = sc.nextInt();
-                int matrix1_3 = sc.nextInt();
-                int matrix1_4 = sc.nextInt();
-                System.out.println("Matrix B:");
-                int matrix2_1 = sc.nextInt();
-                int matrix2_2 = sc.nextInt();
-                int matrix2_3 = sc.nextInt();
-                int matrix2_4 = sc.nextInt();
-//clear the console
 
-                int pair1 = matrix1_1 + matrix2_1;
-                int pair2 = matrix1_2 + matrix2_2;
-                int pair3 = matrix1_3 + matrix2_3;
-                int pair4 = matrix1_4 + matrix2_4;
+        System.out.println("Welcome press y to continue...");
+        String enter = sc.next();
+        if (enter.equalsIgnoreCase("y")) {
 
-                System.out.println("[ " + matrix1_1 + "+" + matrix2_1 + "=" + pair1 + ";" + matrix1_2 + "+" + matrix2_2 + "=" + pair2 + ";" + " ]");
-                System.out.println("[ " + matrix1_3 + "+" + matrix2_3 + "=" + pair3 + ";" + matrix1_4 + "+" + matrix2_4 + "=" + pair4 + ";" + " ]");
+            for (int i = 0; i <= 1; i++) {
 
-                System.out.println("[" + pair1 + " " + pair2 + "]");
-                System.out.println("[" + pair3 + " " + pair4 + "]");
+                Actions(sc);
+                System.out.println("Do you want to do another calculation? If yes enter Y otherwise enter any character.");
+                String conf = sc.next();
+                if (conf.equals("Y") || conf.equals("y")) {
+                    i--;
+
+                } else {
+                    i = 1;
+                }
             }
-            else{
-                break;
-            }
+        } else {
+            System.exit(0);
         }
 
+    }
+
+    public static void Actions(Scanner sc) {
+        System.out.println("Enter matrix data seperated with space( each matrix require 4 digit required).You should enter the digit from left to right");
+        System.out.println("Matrix A:");
+        int matrix1_1 = sc.nextInt();
+        int matrix1_2 = sc.nextInt();
+        int matrix1_3 = sc.nextInt();
+        int matrix1_4 = sc.nextInt();
+        System.out.println("Matrix B:");
+        int matrix2_1 = sc.nextInt();
+        int matrix2_2 = sc.nextInt();
+        int matrix2_3 = sc.nextInt();
+        int matrix2_4 = sc.nextInt();
+//clear the console
+
+        int pair1 = matrix1_1 + matrix2_1;
+        int pair2 = matrix1_2 + matrix2_2;
+        int pair3 = matrix1_3 + matrix2_3;
+        int pair4 = matrix1_4 + matrix2_4;
+
+        System.out.println("[ " + matrix1_1 + "+" + matrix2_1 + "=" + pair1 + ";" + matrix1_2 + "+" + matrix2_2 + "=" + pair2 + ";" + " ]");
+        System.out.println("[ " + matrix1_3 + "+" + matrix2_3 + "=" + pair3 + ";" + matrix1_4 + "+" + matrix2_4 + "=" + pair4 + ";" + " ]");
+
+        System.out.println("[" + pair1 + " " + pair2 + "]");
+        System.out.println("[" + pair3 + " " + pair4 + "]");
     }
 }
